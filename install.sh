30d9f3cd5c61b52ba39ee4ba8ff449ed922851b7
David Aerne <meodai@gmail.com>
2016-09-27 12:02:24 +0200

    adds a SketchPlugins file
    and installes the plugins

--- install.sh
+++ install.sh
@@ -1,18 +1,30 @@
 #!/usr/bin/env bash
 
-if [ $# -eq 0 ]
-  then
-    echo "Make sure to call install.sh yourAppleId@whatever.com"
-fi
+# make sure your Apple ID was set as an argument
+# if [ $# -eq 0 ]
+#   then
+#     echo "Make sure to call install.sh yourAppleId@whatever.com"
+# fi
 
-# install homebrew
-/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)";
+# # install homebrew
+# /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)";
 
-# installs bundle Bundle
-brew tap Homebrew/bundle;
-brew install mas;
+# # installs bundle Bundle
+# brew tap Homebrew/bundle;
+# # installs mas (App store repository)
+# brew install mas;
+# # signs MAS in with your apple store ID
+# mas signin $1;
 
-mas signin $1;
+# # install apps and tool from Brewfile
+# brew bundle;
 
-# restore installed apps
-brew bundle;
+# install chest for Sketch
+gem install chest;
+
+# install Sketch plugins from 'SketchPlugins' file
+while read REPO
+do
+    chest install $REPO
+    echo 'installing ' $REPO 'for Sketch';
+done < SketchPlugins

1463841292a6102859f50d57208c6b3f81b75c30
David Aerne <meodai@gmail.com>
2016-12-04 15:29:04 +0100

    adds random logo on click

--- main.js
+++ main.js
@@ -1,15 +1,17 @@
-const logos = 33;
-const $d = document;
-const $logo = $d.querySelector('.js-logo');
-const logoHeight = 333;
-let position = 1;
+var logos = 33;
+var $d = document;
+var $logo = $d.querySelector('.js-logo');
+var logoHeight = 333;
+var position = 1;
 
-setInterval(() => {
-    if (position < logos) {
-        position++;
-    } else {
-        position = 1;
-    }
-    console.log(`0 ${(position - 1) * -logoHeight}px`)
-    $logo.style['background-position'] = `0 ${((position - 1) * -logoHeight) + 50}px`
-},2000);
+function randomLogoPosition () {
+    return Math.floor(Math.random() * logos);
+}
+
+function setLogo () {
+    $logo.style['background-position'] = `0 ${(randomLogoPosition() * -logoHeight) + 50}px`;
+}
+
+setLogo();
+
+document.addEventListener('click', setLogo);
